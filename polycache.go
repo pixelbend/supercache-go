56cbebcf0cb9105e48c57b1055829a627dc69887
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2024-07-08 21:15:11 +0530

    feat: implemented a common interface minimum amount of functionality

---
+++ polycache.go
@@ -0,0 +1,32 @@
+package polycache
+
+import (
+	"context"
+	"time"
+)
+
+type IPolyCache interface {
+	Set(ctx context.Context, key string, value string, expiry time.Duration) error
+	Get(ctx context.Context, key string) (string, error)
+	Delete(ctx context.Context, key string) error
+}
+
+type PolyCache struct {
+	provider IPolyCache
+}
+
+func New(provider IPolyCache) *PolyCache {
+	return &PolyCache{provider: provider}
+}
+
+func (p *PolyCache) Set(ctx context.Context, key string, value string, expiry time.Duration) error {
+	return p.provider.Set(ctx, key, value, expiry)
+}
+
+func (p *PolyCache) Get(ctx context.Context, key string) (string, error) {
+	return p.provider.Get(ctx, key)
+}
+
+func (p *PolyCache) Delete(ctx context.Context, key string) error {
+	return p.provider.Delete(ctx, key)
+}
